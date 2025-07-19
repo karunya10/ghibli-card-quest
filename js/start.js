388e057746cb126bb847f483765c2705b5bfea13
karunya10 <karunya.prabha@gmail.com>
2025-07-19 10:43:54 +0200

    Implementation of Screen3

--- js/start.js
+++ js/start.js
@@ -6,4 +6,12 @@ window.onload = function () {
     document.querySelector("#game-second-screen").style.display = "flex";
     document.body.style.backgroundImage = "url('../images/dust-mites1.png')";
   }
+
+  const difficultyButton = document.querySelector("#easy");
+  difficultyButton.addEventListener("click", showThirdScreen);
+  function showThirdScreen() {
+    document.querySelector("#game-second-screen").style.display = "none";
+    document.querySelector("#game-third-screen").style.display = "flex";
+    document.body.style.backgroundImage = "url('../images/train.png')";
+  }
 };
